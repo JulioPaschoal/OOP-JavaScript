36dcbc7f2666821ac5684a6c83ff20445aed4b38
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2023-01-16 18:42:16 -0300

    Aprimorando mais conhecimento sobre javaScript

--- 16_instanceof/index.js
+++ 16_instanceof/index.js
@@ -1,1 +1,14 @@
+function Robo(nome, arma) {
+  this.nome = nome;
+  this.arma = arma;
+}
+
+function Humano(nome) {
+  this.nome = nome;
+}
+
+let android = new Robo('Xyz', 'punhos');
+
+console.log(android instanceof Robo);
+console.log(android instanceof Humano);
 
